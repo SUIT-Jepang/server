4fb528620340d81ce2c0367318bdece02c6e8b16
rozakus <abdulrozak.mail@gmail.com>
2021-01-22 12:43:15 +0700

    setup server

--- app.js
+++ app.js
@@ -3,93 +3,36 @@ const server = require('http').createServer(app)
 const io = require("socket.io")(server)
 const PORT = 3000
 
-const {createRoom, joinRoom, exitRoom, rooms} = require('./util/rooms')
-const {userConnected, connectedUser, initializeChoice, makeMove, moves, choices} = require('./util/users')
-let roomProperty = {}
-io.on("connect", (socket) => {
-    console.log('connect')
-    socket.on('createRoom', (roomId) => {
-        if(rooms[roomId]){
-            const error = 'this room already exists'
-            socket.emit('error-create-room', error)
-        }
-        else {
-            userConnected(socket.client.id)
-            createRoom(roomId, socket.client.id)
-            socket.emit('room-joined', roomId)
-            socket.emit('player-1-connected')
-            socket.join(roomId)
-        }
-        console.log(rooms)
-    })
-    socket.on('joinRoom', (roomId) => {
-        console.log('masuk')
-        if(!rooms[roomId]){
-            const error = "This room doen't exist"
-            socket.emit('error-join-room', error)
-        }
-        else {
-            userConnected(socket.client.id)
-            createRoom(roomId, socket.client.id)
-            socket.emit('room-created', roomId)
-            socket.emit('player-1-connected')
-            socket.join(roomId)
-            console.log(rooms)
-        }
-        console.log(rooms)
-    })
-    socket.on('join-random', () => {
-        let roomId = ''
-        for(let id in rooms){
-            if(rooms[id][1] === ''){
-                roomId = id
-                break
-            }
-        }
+let data = {
+    user: []
+}
 
-        if(roomId === '') {
-            const error = 'all room are full or none exist'
-            socket.emit('error', error)
-        }
-        else {
-            userConnected(socket.client.id)
-            createRoom(roomId, socket.client.id)
-            socket.emit('room-created', roomId)
-            socket.emit('player-1-connected')
-            socket.join(roomId)
-        }
+io.on("connection", (socket) => {
+    console.log('Socket.Io is connected')
+
+    socket.emit('init', { message: 'Ini dari server' })
+
+    socket.on('choose', function (payload) {
+        console.log(payload)
+        io.emit('choose', payload)
     })
-    socket.on('make-move', ({playerId, myChoice, roomId}) => {
-        makeMove(roomId, playerId, myChoice)
-        if(choices[roomId][0] !== '' && choices[roomId][1] === ''){
-            let playerOne = choices[roomId][0];
-            let playerTwo = choices[roomId][1];
-            if(playerOne === playerTwo){
-                let message = 'Both of you chose ' +  playerOne + ' . So its draw'
-                io.to(roomId).emit('draw', message)
-            } else if (moves[playerOne] === playerTwo){
-                let enemyChoice = ''
-                if(playerId === 1){
-                    enemyChoice = playerTwo
-                }
-                else {
-                    enemyChoice = playerOne
-                }
-                choices[rooms] = ['', '']
-                io.to(roomId).emit('players-1-wins', {myChoice, enemyChoice})
+
+    socket.on('login', function (payload) {
+        console.log('login')
+        if (data.user.length <= 2) {
+
+            if (data.user.length === 0) {
+                data.user.push({ player: `player1`, name: payload })
+                socket.emit('login', data.user[0])
+                console.log('>>> user login ', data.user)
             } else {
-                let enemyChoice = ''
-                if(playerId === 1){
-                    enemyChoice = playerTwo
-                }
-                else {
-                    enemyChoice = playerOne
-                }
-                choices[rooms] = ['', '']
-                io.to(roomId).emit('players-2-wins', {myChoice, enemyChoice})
+                data.user.push({ player: `player2`, name: payload })
+                socket.emit('login', data.user[1])
+                console.log('>>> user login ', data.user)
             }
-        }
+            // io.emit('login', payload)
 
+        }
     })
 })
 
